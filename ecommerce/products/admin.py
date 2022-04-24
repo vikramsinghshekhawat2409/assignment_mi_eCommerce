5b2aaa304d3cb10ec4bfe9f1538d6d03fdae4df9
vikramsinghshekhawat2409 <vikramss0264@gmail.com>
2022-04-24 10:06:06 +0530

    add:: models, adminModels, db, migrations

--- ecommerce/products/admin.py
+++ ecommerce/products/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    ordering = ('id',)
+
+
+class SubCategoriesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'get_category_name')
+    ordering = ('id',)
+
+    def get_category_name(self, obj):
+        return obj.category.name
+
+    get_category_name.short_description = 'Category'
+
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ('id','name','get_sub_category_name', 'get_category_name')
+    ordering = ('id',)
+
+    def get_sub_category_name(self, obj):
+        return obj.subcategory.name
+
+    def get_category_name(self, obj):
+        return obj.subcategory.category.name
+
+    get_category_name.short_description = 'Category'
+    get_sub_category_name.short_description = 'SubCategory'
--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+
 # Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        db_table = "categories"
+
+    def __str__(self):
+        return self.name
+
+
+class SubCategories(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, related_name='category')
+
+    class Meta:
+        db_table = "sub_categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=100)
+    subcategory = models.ForeignKey(SubCategories, on_delete=models.CASCADE, related_name='subcategory')
+
+    class Meta:
+        db_table = "products"
+
+    def __str__(self):
+        return self.name
